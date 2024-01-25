7f3bc954b2a706d76c32c92add9f1128421289a5
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-01-25 22:08:53 +0100

    bouton test montre l'heure

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cuculidae
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -18,12 +20,26 @@ namespace Cuculidae
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                clicked.Text = DateTime.Now.Hour.ToString() + " : " + DateTime.Now.Minute.ToString() + " : " + DateTime.Now.Second.ToString();
+            }
+        }
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clicked.Text = DateTime.Now.Hour.ToString() + " : " + DateTime.Now.Minute.ToString() + " : " + DateTime.Now.Second.ToString();
         }
     }
 }
