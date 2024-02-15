89e64a3d418ce22d3753c97911b36e3c4969589e
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-02-15 23:36:01 +0100

    heure transmise dans le background worker (test s'il est bien 23h33 et 15 seconde par exemple)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,15 +1,6 @@
 ﻿using System.ComponentModel;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Threading;
 
 namespace Cuculidae
 {
@@ -18,28 +9,83 @@ namespace Cuculidae
     /// </summary>
     public partial class MainWindow : Window
     {
+        BackgroundWorker worker = new BackgroundWorker();
+        System.Windows.Threading.DispatcherTimer Timer = new System.Windows.Threading.DispatcherTimer();
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();
+
+            // DoWork handler
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            // ProgressChanged handler
+            worker.ProgressChanged += worker_ProgressChanged;
+            // WorkerCompleted handler
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+
+
+            Timer.Tick += new EventHandler(TimerUpdate);
+
+            Timer.Interval = new TimeSpan(0, 0, 1);
+
+            Timer.Start();
+
         }
 
-        private readonly BackgroundWorker worker = new BackgroundWorker();
+
+        private void TimerUpdate(object sender, EventArgs e)
+
+        {
+            Console.WriteLine(clicked.Text);
+            DateTime d;
+            d = DateTime.Now;
+            clicked.Text= d.Hour + " : " + d.Minute + " : " + d.Second;
+            worker.RunWorkerAsync(argument: clicked.Text);
+
+        }
 
         private void worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            while (true)
+            Console.WriteLine("worker checking");
+            bool contains = false;
+            String hour = (string)e.Argument;
+            Console.WriteLine("hour : "+ hour);
+            Console.WriteLine(hour.Length);
+            if (hour == "23 : 33 : 15") { contains = true; };
+/*            for (int i = 0; i < hour.Length; i++)
             {
-                clicked.Text = DateTime.Now.Hour.ToString() + " : " + DateTime.Now.Minute.ToString() + " : " + DateTime.Now.Second.ToString();
+                if (hour[i] == '2') { contains = true; };
+            }*/
+            if (contains == true)
+            {
+                Console.WriteLine("L'heure est bien 23 : 33 : 15");
             }
+
         }
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
 
         }
 
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            clicked.Text = DateTime.Now.Hour.ToString() + " : " + DateTime.Now.Minute.ToString() + " : " + DateTime.Now.Second.ToString();
+            worker.RunWorkerAsync();
+
+            //if (Timer.IsEnabled)
+            //{
+            //    Timer.Stop();
+            //}
+            //else
+            //{
+            //    Timer.Start();
+            //}
         }
     }
 }
