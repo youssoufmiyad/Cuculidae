e880a258206266917e0864de7894208162b596df
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-03-02 22:34:57 +0100

    Création du controlleur de choix d'heure pour l'alarme

---
+++ AlarmSetter.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Cuculidae
+{
+    /// <summary>
+    /// Logique d'interaction pour AlarmSetter.xaml
+    /// </summary>
+    public partial class AlarmSetter : UserControl
+    {
+        public AlarmSetter()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]");
+        }
+
+        private void Hour_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            string previous_hour = textBox.Text;
+
+            Console.WriteLine("TEXTBOX TEXT : "+textBox.Text);
+            if (textBox.Text.Length > 2)
+            {
+                textBox.Text = previous_hour;
+            }
+            
+        }
+        private void Minute_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Second_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
